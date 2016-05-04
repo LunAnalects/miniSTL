d9cdc4419ea18bd3980f745f1be381ebd5c48c86
zhanglunzhao <lunanalects@outlook.com>
2016-05-04 20:15:47 +0800

    starting new parts, not finished yet.

---
+++ src/iterator.hpp
@@ -0,0 +1,42 @@
+﻿#ifndef MINISTL_ITERATOR_HPP
+#define MINISTL_ITERATOR_HPP
+
+namespace ministl
+{
+	template<typename T>
+	class iterator_traits
+	{
+		using value_type		= typename T::value_type;
+		using difference_type	= typename T::difference_type;
+		using pointer			= typename T::pointer;
+		using reference			= typename T::reference;
+		using iterator_category = typename T::iterator_category;
+
+	};
+
+	template<typename T>
+	class iterator_traits<T *>
+	{
+		using value_type		= typename T::value_type;
+		using difference_type	= typename T::difference_type;
+		using pointer			= typename T::pointer;
+		using reference			= typename T::reference;
+		using iterator_category = typename T::iterator_category;
+
+	};
+
+	template<typename T>
+	class iterator_traits<const T *>
+	{
+		using value_type		= typename T::value_type;
+		using difference_type	= typename T::difference_type;
+		using pointer			= typename T::pointer;
+		using reference			= typename T::reference;
+		using iterator_category = typename T::iterator_category;
+
+	};
+	
+} //ministl
+
+
+#endif //MINISTL_ITERATOR_HPP
---
+++ src/types.hpp
@@ -0,0 +1,10 @@
+#ifndef MINISTL_TYPES_HPP
+#define MINISTL_TYPES_HPP
+#include <cstdint>
+namespace ministl
+{
+	using size_t = uint64_t;
+
+}
+
+#endif //MINISTL_TYPES_HPP
